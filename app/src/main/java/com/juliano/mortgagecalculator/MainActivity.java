85f3575de9fcc159ef52436225fb5bf81e397930
JulianoF <juliano1@live.ca>
2024-10-01 11:51:32 -0400

    Completed Desired UI

--- app/src/main/java/com/juliano/mortgagecalculator/MainActivity.java
+++ app/src/main/java/com/juliano/mortgagecalculator/MainActivity.java
@@ -1,7 +1,10 @@
 package com.juliano.mortgagecalculator;
 
 import android.os.Bundle;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar topToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,18 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        topToolBar = (Toolbar) findViewById(R.id.topToolBar);
+        setSupportActionBar(topToolBar);
+        getSupportActionBar().setTitle("Mortgage Calculator");
+
+        Spinner spinner = (Spinner) findViewById(R.id.paymentSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.payment_options,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
     }
 }
